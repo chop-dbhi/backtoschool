0cb3ef400eea2f0bcdf8323d7b3c503f8e2f05bb
l-k- <alenka0000@hotmail.com>
2015-05-17 14:20:05 -0400

    missed a few more tabs..

--- imgEditor/editor/processing/sepia.py
+++ imgEditor/editor/processing/sepia.py
@@ -12,13 +12,13 @@ def applyFilter(pixels):
 
     for pixel in pixels:
 
-	(inputRed, inputGreen, inputBlue)  = pixel
+        (inputRed, inputGreen, inputBlue)  = pixel
 
         outputRed = min(255, int((inputRed * .393) + (inputGreen *.769) + (inputBlue * .189)))
         outputGreen = min(255, int((inputRed * .349) + (inputGreen *.686) + (inputBlue * .168)))
         outputBlue = min(255, int((inputRed * .272) + (inputGreen *.534) + (inputBlue * .131)))
 
-	newPixel = (outputRed,outputGreen,outputBlue)
+        newPixel = (outputRed,outputGreen,outputBlue)
         sepiaPixels.append(newPixel)
 
     return sepiaPixels
