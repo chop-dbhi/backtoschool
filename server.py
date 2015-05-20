3c6ed81505a2eb817ecac888d70c1ff9707bd0c1
Jeff Miller <jeffmax@gmail.com>
2015-05-19 20:29:49 -0400

    Rename server.py to cgi_server.py.
    Add plain web server as server.py

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
- 
-import BaseHTTPServer
-import CGIHTTPServer
-import cgitb; cgitb.enable()  ## This line enables CGI error reporting
- 
-server = BaseHTTPServer.HTTPServer
-handler = CGIHTTPServer.CGIHTTPRequestHandler
-server_address = ("", 8000)
-handler.cgi_directories = ["/"]
- 
-httpd = server(server_address, handler)
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
 httpd.serve_forever()
