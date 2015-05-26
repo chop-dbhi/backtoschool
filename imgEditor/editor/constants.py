a7e83a212a126448c18b6ff15f710f4c1969f008
l-k- <alenka0000@hotmail.com>
2015-05-26 15:48:45 -0400

    updated the publish directory for use with editr

--- imgEditor/editor/constants.py
+++ imgEditor/editor/constants.py
@@ -4,7 +4,7 @@ import sys
 
 APP_NAME = 'Biomedical Image Processing Tutorial'
 BASEDIR = os.path.dirname(os.path.abspath(sys.argv[0]))
-PUBLISH_DIR = BASEDIR
+PUBLISH_DIR = PUBLISH_DIR = os.path.join(os.path.join(BASEDIR, '..'),'img')
 
 osName = platform.system().lower()
 IS_MAC = (osName == 'darwin')
