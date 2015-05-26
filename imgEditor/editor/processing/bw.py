0b860c9a603ce35f8a008461020540029db31eb8
l-k- <alenka0000@hotmail.com>
2015-05-26 18:30:06 -0400

    updated the image processing code
    - added better comments to the image filter code;
    - removed the solution from the x-ray processing code; added
    instructions instead
    - modified lateral.png so that the brightest pixels (i.e. the pin) have
    R=G=B=253 (used to be 255, probably too easy to find?)

--- imgEditor/editor/processing/bw.py
+++ imgEditor/editor/processing/bw.py
@@ -1,21 +1,45 @@
 def applyFilter(pixels):
 
-    '''The simplest method of converting to B&W is to average the values: R = G = B = (R + G + B) / 3'''
+    # This is an array where we'll store pixels for the new image. In the beginning, it's empty.
+    newPixels = []
 
-    bwPixels = []
-
+    # Let's go through the entire image, one pixel at a time
     for pixel in pixels:
 
-        (inputRed, inputGreen, inputBlue)  = pixel
+        # Let's get the Red, Green and Blue values for the current pixel
+        inputRed  = pixel[0]
+        inputGreen = pixel[1]
+        inputBlue = pixel[2]
 
-        outputRed = outputGreen = outputBlue = (inputRed + inputGreen + inputBlue) / 3
+       # in a Black&White image, each pixel has the same value for Red, Green, and Blue.
+       # But what is that value?  
+       # Red = Green = Blue = ?
+       # The easiest method of calculating the new value is to average the old values: 
+       # newRed = newGreen = newBlue = (oldRed + oldGreen + oldBlue) / 3
 
-        #outputRed = min(outputRed+50, 255)  # uncomment to increase the red levels and make the image
-                                                                        # red-and-white instead of b&w. The new value needs to be capped at 255.
-        #outputGreen = min(outputGreen*2, 255)  # green-and-white
-        #outputBlue = min(outputBlue*2, 255)  # blue-and-white
+	average = (inputRed + inputGreen + inputBlue) / 3
 
-        newPixel = (outputRed, outputGreen, outputBlue)
-        bwPixels.append(newPixel)
+        outputRed = average
+        outputGreen = average
+        outputBlue = average
 
-    return bwPixels
+        # Now that we know the color values for the new pixel, let's create this pixel. 
+        newPixel = [0,0,0]
+
+        # Let's set the Red, Green, and Blue values for this new pixel
+        newPixel[0] = outputRed 
+        newPixel[1] = outputGreen
+        newPixel[2] = outputBlue
+
+        # Optional exercise: 
+        # Now that you know how to convert an image to B&W, try something new!
+        # Try changing the output values. For example, If you increase the outputRed value, 
+        # you can make the image red-and-white instead of black-and-white!
+        # But remember that each color value cannot be bigger than 255.
+        # To make sure that a number is not bigger than 255, you can use the min function, for example:
+        # newNumber = min(number, 255)
+
+	# add the new pixel to the resulting image
+        newPixels.append(newPixel)
+
+    return newPixels
--- imgEditor/editor/processing/xray.py
+++ imgEditor/editor/processing/xray.py
@@ -1,16 +1,35 @@
 def applyFilter(pixels):
 
+    # This is an array where we'll store pixels for the new image. In the beginning, it's empty.
     newPixels = []
 
+    # Let's go through the entire image one pixel at a time
     for pixel in pixels:
+        
+        # Let's get the Red, Green and Blue values for the current pixel
+        inputRed  = pixel[0]
+        inputGreen = pixel[1]
+        inputBlue = pixel[2]
 
-        (r,g,b)  = pixel
+        # create a new pixel
+        newPixel = [0,0,0]
 
-        if r>250 and g>250 and b>250:
-            g=0
-            b=0
+        # Calculate new Red, Green and Blue values for this pixel.
+        # Look at the code below: right now all we are doing is keeping the old values. 
+        # How can we change these values to make the pin more visible? 
+        # Remember, the pin was very dense, so the pixels corresponding to it 
+        # will be very bright in the X-ray image. 
+        # The maximum value for each color is 255, so the brightest possible pixel has 
+        # Red = Green = Blue = 255.
+        # A pixel that is almost as bright but a little dimmer will have Red = Green = Blue = 254;
+        # an even dimmer one will have Red = Green = Blue = 253, and so on. 
+        # Try to guess the color values for the brightest pixels in this X-Ray image, and you will find the pin!
+        # When you find the brightest pixels, change their color to red, so that we can see the pin in the X-ray. 
 
-        newPixel = (r,g,b)
+        newPixel[0] = inputRed
+        newPixel[1] = inputGreen
+        newPixel[2] = inputBlue
+
         newPixels.append(newPixel)
 
     return newPixels
