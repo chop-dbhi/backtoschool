f635e784b560b69df205d053ec7a7d46d629d825
Jeffrey Miller <millerjm1@email.chop.edu>
2015-05-27 14:35:35 -0400

    Fix comment

--- projects/backtoschool/main.js
+++ projects/backtoschool/main.js
@@ -6,7 +6,7 @@ var button = document.createElement("button");
 button.id = "pin";
 button.innerHTML = "Find Pin";
 
-// This sets up a code that gets called if the user clicks on the button
+// This sets up code (a function) that gets executed if the user clicks on the button
 button.onclick = function(){
   var img = document.getElementById('xray');
   img.src = '/backtoschool/img/lateral_pin.png';
